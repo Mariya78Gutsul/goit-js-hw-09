73dc4210ec4c54a3d7877f77aa786a9965a3d9f2
Mariya78Gutsul <mariyagutsui1@gmail.com>
2021-11-16 19:56:21 +0200

    the first job

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
 
+
+function getRandomHexColor() {
+   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+ }
+ const btnStart = document.querySelector("button[data-start]");
+ const btnStop = document.querySelector("button[data-stop]");
+ const body = document.querySelector("body");
+ let timerId = null;
+
+ btnStart.addEventListener("click", () => {
+    body.classList.add("background-color");
+    btnStart.disabled = true;
+    timerId = setInterval(() => {
+       body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+ });
+ btnStop.addEventListener("click", () => {
+   btnStart.disabled = false;
+   clearInterval(timerId) 
+ });
